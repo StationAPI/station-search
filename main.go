80d9590b88f607e9527e5f85f1d026ceff51524b
FerretCode <674313@salemkeizer.org>
2023-03-24 22:28:50 +0000

    finish search service

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/stationapi/station-search/db"
+	neon "github.com/stationapi/station-search/db"
 )
 
+type response struct {
+	Websites []neon.Website
+}
+
 func main() {
-	db, err := db.Connect()
+	db, err := neon.Connect()
 
 	if err != nil {
 		log.Fatal(err)
@@ -24,5 +29,21 @@ func main() {
 	r.Get("/search", func(w http.ResponseWriter, r *http.Request) {
 		search := r.URL.Query().Get("search")
 
+		websites := neon.SearchWebsites(search, db)
+
+		res := response{
+			Websites: websites,
+		}
+
+		stringified, err := json.Marshal(res)
+
+		if err != nil {
+			http.Error(w, "there was an error searching for websites", http.StatusInternalServerError)
+
+			return
+		}
+
+		w.WriteHeader(200)
+		w.Write(stringified)
 	})
 }
